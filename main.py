cf30147147029170aa8bbfd3ce05268ae0307bcb
charisgao <charis.m.gao@gmail.com>
2021-07-08 15:38:28 +0000

    brunch

---
+++ main.py
@@ -0,0 +1,5 @@
+#pricing, type of food (brunch/restaurant/etc), location, cuisine
+#if there are recommendations, would you rather eat __ or ___
+#suggest restaurant
+
+preference = input("What type of food would you prefer to eat: Brunch, Cafes, Fast Food, Desserts, Restaurants? ")
