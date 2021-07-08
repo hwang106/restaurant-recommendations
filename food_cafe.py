f956d2a0efa791ccff8bec1eb2376ab8632f0b04
charisgao <charis.m.gao@gmail.com>
2021-07-08 18:14:26 +0000

    update

---
+++ food_cafe.py
@@ -0,0 +1,23 @@
+cafe = [
+        {"Name": "Matto Expresso", "Type": "coffee shop", "Price range": "$", "Location": "east village"},
+        {"Name": "Elsewhere Espresso", "Type": "coffee shop", "Price range": "$", "Location": "east village"},
+        {"Name": "Prince Tea House", "Type": "tea house", "Price range": "$$", "Location": "east village"},
+        {"Name": "Café Kitsune", "Type": "coffee shop", "Price range": "$$", "Location": "west village"},
+        {"Name": "Café Select", "Type": "cafe", "Price range": "$$", "Location": "soho"},
+        {"Name": "Juice Press", "Type": "juices", "Price range": "$$", "Location": "multiple locations"},
+        {"Name": "Maman", "Type": "cafe", "Price range": "$$", "Location": "soho"},
+        {"Name": "Think Coffee", "Type": "coffee shop", "Price range": "$", "Location": "multiple locations"},
+        {"Name": "Saltwater Coffee", "Type": "coffee shop", "Price range": "$", "Location": "east village"},
+        {"Name": "Irving Farm", "Type": "coffee shop", "Price range": "$", "Location": "multiple locations"},
+        {"Name": "Le Fournil", "Type": "bakery", "Price range": "$$", "Location": "east village"},
+        {"Name": "Fabrique", "Type": "bakery", "Price range": "$$", "Location": "meatpacking"},
+        {"Name": "Debutea", "Type": "boba", "Price range": "$", "Location": "greenwich village"},
+        {"Name": "Blue Bottle Coffee", "Type": "coffee shop", "Price range": "$$", "Location": "multiple locations"},
+        {"Name": "Bluestone Lane", "Type": "cafe", "Price range": "$$", "Location": "multiple locations"},
+        {"Name": "Chobani SoHo", "Type": "cafe", "Price range": "$$", "Location": "soho"} ,
+        {"Name": "Bibble & Sip", "Type": "cafe", "Price range": "$", "Location": "little italy"},
+        {"Name": "Devocion", "Type": "coffee shop", "Price range": "$", "Location": "flatiron"},
+        {"Name": "Dr Smood", "Type": "coffee shop", "Price range": "$$", "Location": "soho"},
+        {"Name": "Ground Support Coffee", "Type": "coffee shop", "Price range": "$", "Location": "soho"},
+        {"Name": "Alimama", "Type": "coffeeshop", "Price range": "$$", "Location": "chinatown"}
+]
--- main.py
+++ main.py
@@ -3,9 +3,18 @@ import food_brunch
 #suggest restaurant
 
 preference = input("What type of food would you prefer to eat: Brunch, Cafes, Fast Food, Desserts, Restaurants? ")
+
 if preference.lower() == "brunch":
-    cuisine = input("What type of cuisine would you prefer: American, French, Irish, Italian, Seafood, Spanish? ")
-    price = input("What price range would you prefer: $, $$, $$$, $$$$? ")
-    for i in range(len(food_brunch.brunch)):
-        if cuisine.lower() == food_brunch.brunch[i]["cuisine"] and price.lower() == food_brunch.brunch[i]["pricing"]:
-            print(food_brunch.brunch[i]["name"] + " in " + food_brunch.brunch[i]["location"].title())
+    choice = input("Would you like to choose your restaurant by cuisine or location? ")
+    if choice.lower() == "cuisine":
+        cuisine = input("What type of cuisine would you prefer: American, French, Irish, Italian, Seafood, Spanish? ")
+        price = input("What price range would you prefer: $, $$, $$$, $$$$? ")
+        for i in range(len(food_brunch.brunch)):
+            if cuisine.lower() == food_brunch.brunch[i]["cuisine"] and price.lower() == food_brunch.brunch[i]["pricing"]:
+                print(food_brunch.brunch[i]["name"] + " in " + food_brunch.brunch[i]["location"].title())
+    if choice.lower() == "location":
+        location = input("What location would you like to eat at: Alphabet City, Chelsea, East Village, Greenwich Village,\nLittle Italy, Lower East Side, Meatpacking, Midtown, SoHo, Upper East Side, West Village, Williamsburg? ")
+        price = input("What price range would you prefer: $, $$, $$$, $$$$? ")
+        for i in range(len(food_brunch.brunch)):
+            if location.lower() == food_brunch.brunch[i]["location"] and price.lower() == food_brunch.brunch[i]["pricing"]:
+                print(food_brunch.brunch[i]["name"] + " (" + food_brunch.brunch[i]["cuisine"].title() + " food)")
